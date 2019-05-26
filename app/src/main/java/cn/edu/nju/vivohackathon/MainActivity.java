545a0c4f15068a1aca24a0e0ce784680b64c998e
BalladeNo1InGMinorOp23 <976792132@qq.com>
2019-05-26 23:19:16 +0800

    2319

--- app/src/main/java/cn/edu/nju/vivohackathon/MainActivity.java
+++ app/src/main/java/cn/edu/nju/vivohackathon/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        BottomNavigationView navView = findViewById(R.id.discover_nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
