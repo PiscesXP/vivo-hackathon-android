246a937b4092822146e15df592a786057b4f6fbf
Zhenxi <PiscesXP@outlook.com>
2019-05-26 20:47:26 +0800

    更详细的网络请求 Log.

--- app/src/main/java/cn/edu/nju/vivohackathon/tools/network/powerpost/PowerPostCallback.java
+++ app/src/main/java/cn/edu/nju/vivohackathon/tools/network/powerpost/PowerPostCallback.java
@@ -26,7 +26,7 @@ public abstract class PowerPostCallback {
             @Override
             public void onSucc(Response response) {
                 if (!response.isSuccessful()) {
-                    Log.i(TAG, "Response not successful: " + url);
+                    Log.e(TAG, "Response not successful: " + url);
                     onFail("Response unsuccessful");
                     return;
                 }
@@ -35,25 +35,27 @@ public abstract class PowerPostCallback {
                     JSONObject resultJson;
                     if (responseBody != null) {
                         String responseText = responseBody.string();
-                        Log.i(TAG, "Response text:" + responseText);
+                        Log.i(TAG, String.format("Response text from %s: %s", url, responseText));
                         resultJson = JSONObject.parseObject(responseText);
                     } else {
+                        Log.i(TAG, String.format("Response from %s without body.", url));
                         resultJson = new JSONObject();
                     }
                     onSuccess(resultJson);
                 } catch (Exception e) {
-                    Log.e(TAG, e.toString());
+                    Log.e(TAG, String.format("Exception on parsing json from %s: %s", url, e.toString()));
                     onFail(e.toString());
                 }
             }
 
             @Override
             public void onError(String errorMsg) {
-                Log.e(TAG, "Error on request " + url + ", errorMessage:" + errorMsg);
+                Log.e(TAG, String.format("Error on request %s, errorMessage:%s", url, errorMsg));
                 if (++mRetry <= MAX_RETRIES) {
                     Log.i(TAG, String.format("Retrying %s... (%d/%d)", url, mRetry, MAX_RETRIES));
                     sendRequest(context, url, requestJson);
                 } else {
+                    Log.e(TAG, String.format("Request %s failed after %d retries.", url, mRetry));
                     onFail(errorMsg);
                 }
             }
