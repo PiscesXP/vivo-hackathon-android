6368ee85ff2c43c48f44bf38b828175196af4a8d
Zhenxi <PiscesXP@outlook.com>
2019-05-26 16:13:30 +0800

    删除旧代码.

--- app/src/main/java/cn/edu/nju/vivohackathon/MainActivity.java
+++ app/src/main/java/cn/edu/nju/vivohackathon/MainActivity.java
@@ -1,7 +1,5 @@
 package cn.edu.nju.vivohackathon;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -13,14 +11,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.alibaba.fastjson.JSONObject;
-
 import cn.edu.nju.vivohackathon.businesslogic.account.UserInfo;
-import cn.edu.nju.vivohackathon.tools.network.HttpRequestCallback;
-import cn.edu.nju.vivohackathon.tools.network.HttpRequest;
-import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity implements HttpRequestCallback {
+public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
 
     private UserInfo mUserInfo;
@@ -55,70 +48,22 @@ public class MainActivity extends AppCompatActivity implements HttpRequestCallba
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        UserInfo userInfo = new UserInfo(getApplicationContext(),this);
+        mUserInfo = UserInfo.getInstance(getApplicationContext(), this);
 
-        //读取已保存的账号密码
-        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        String savedUsername = sharedPreferences.getString(getString(R.string.preference_username), "");
-        String savedPassword = sharedPreferences.getString(getString(R.string.preference_password), "");
-        EditText etUsername = findViewById(R.id.etUsername);
-        etUsername.setText(savedUsername);
-        EditText etPassword = findViewById(R.id.etPassword);
-        etPassword.setText(savedPassword);
-
-        //set listeners
+        //设置listeners
         Button loginButton = findViewById(R.id.btnLogin);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginButtonClickHandler();
+                String username = ((EditText) findViewById(R.id.etUsername)).getText().toString();
+                String password = ((EditText) findViewById(R.id.etPassword)).getText().toString();
+                if (username.matches("^.{6,16}$") && password.matches("^.{6,16}$")) {
+                    mUserInfo.register(username, password);
+                } else {
+                    Toast.makeText(getApplicationContext(), "账号/密码太短", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
-        Button fetchButton = findViewById(R.id.btnFetch);
-        fetchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fetchButtonClickHandler();
-            }
-        });
-    }
-
-    private void loginButtonClickHandler() {
-        String username = ((EditText) findViewById(R.id.etUsername)).getText().toString();
-        String password = ((EditText) findViewById(R.id.etPassword)).getText().toString();
-        JSONObject loginJson = new JSONObject();
-        loginJson.put("userName", username);
-        loginJson.put("passWord", password);
-        HttpRequest.getInstance(this).post("http://192.168.2.210:8080/app/login", loginJson, this);
-        //保存登录账号密码
-        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        sharedPreferences.edit()
-                .putString(getString(R.string.preference_username), username)
-                .putString(getString(R.string.preference_password), password)
-                .apply();
-    }
-
-    private void fetchButtonClickHandler() {
-        HttpRequest.getInstance(this).get("http://192.168.2.210:8080/test", this);
-    }
-
-
-    @Override
-    public void onSucc(Response response) {
-        try {
-            String responseText = response.body().string();
-            mTextMessage.setText(responseText);
-            Toast.makeText(this, responseText, Toast.LENGTH_LONG).show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    @Override
-    public void onError(String errorMsg) {
-        Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show();
     }
 
 
